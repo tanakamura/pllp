01a357d911751e2bfb670bfd1fe75e8b7a5c1978
tanakmura <tanakmura@gmail.com>
2018-11-09 04:08:36 +0900

    a

--- docs/link/reloc.s
+++ docs/link/reloc.s
@@ -1,14 +1,12 @@
 	.globl	_start
 _start:
-	movl	$ref_32bit, %eax # 32bitアドレスを参照
-	movl	$ref_32bit + 32, %eax # 32bitアドレスを参照
-
-	movq	$ref_64bit, %rax # 32bit or 64bit アドレスを参照
-
+	movl	$ref_32bit, %eax # 32bitラベルを参照
+	movw	$ref_16bit, %ax	 # 16bitラベルを参照
+	movb	$ref_8bit, %al   # 8bitラベルを参照
 	jmp	ref_as_jmp_label # PC 相対アドレスを参照
+
+	movl	$ref_32bit + 32, %eax # オフセット付き
 
 	mov	$60, %rax
 	syscall
 
-	.quad	ref_64bit  	# 64bitアドレスを参照
-	.long	ref_32bit	# 32bitアドレスを参照
---
+++ docs/link/reloc_label.s
@@ -0,0 +1,15 @@
+	.text
+	.globl ref_32bit
+	.globl ref_16bit
+	.globl ref_8bit
+	.globl ref_as_jmp_label
+
+	nop
+ref_32bit:
+	nop
+ref_16bit:
+	nop
+ref_8bit:
+	nop
+ref_as_jmp_label:
+	nop
