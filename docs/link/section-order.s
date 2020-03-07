b1ef2bb684f89fa8eeedb2a2cce4b6d24aa88cd8
Takashi Nakamura <tanakmura@gmail.com>
2020-03-07 19:20:37 +0900

    tmp

--- docs/link/section-order.s
+++ docs/link/section-order.s
@@ -1,18 +1,18 @@
-	.section	section1
+	.section	section0
 label0:
 	.byte	0x41 		# ascii¤Î'A'
 	.byte	0x42
 	.byte	0x43
 	.byte	0x44
 	
-	.section	section2
+	.section	section1
 label1:
 	.byte	0x45
 	.byte	0x46
 	.byte	0x47
 	.byte	0x48
 
-	.section	section1
+	.section	section0
 label2:	
 	.byte	0x49
 	.byte	0x4a
